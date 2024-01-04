5d76ed7a45f187f7916be7cd6bde54a4dae14297
haithemalsaffar <155380799+haithemalsaffar@users.noreply.github.com>
2024-01-03 21:30:11 -0500

    Implemented add()

--- src/b/DemoB.java
+++ src/b/DemoB.java
@@ -3,6 +3,11 @@ package b;
 public class DemoB {
 	public static void main(String[] args) {
 		System.out.println("GitHub on day 41");
+		System.out.println("Hello JAva again");
 	}
-
+public static void add(int a , int b) {
+	System.out.println(a+b);
+		
+	}
 }
+
